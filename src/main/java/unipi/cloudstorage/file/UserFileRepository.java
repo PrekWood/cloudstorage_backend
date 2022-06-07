3ca0342d461ecb231d5c8c5cfb3f0d15263fa26d
PrekWood <nikosprek2000@gmail.com>
2022-06-07 13:22:44 +0300

    minor code cleanup 2

--- src/main/java/unipi/cloudstorage/file/UserFileRepository.java
+++ src/main/java/unipi/cloudstorage/file/UserFileRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import unipi.cloudstorage.file.enums.UserFileField;
 import unipi.cloudstorage.otp.Otp;
 import unipi.cloudstorage.shared.enums.OrderWay;
 import unipi.cloudstorage.user.User;
--- src/main/java/unipi/cloudstorage/file/enums/UserFileField.java
+++
@@ -1,8 +0,0 @@
-package unipi.cloudstorage.file.enums;
-
-public enum UserFileField {
-    id,
-    user_id,
-    file_path,
-    extension
-}
